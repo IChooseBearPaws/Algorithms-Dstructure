a5c224a37400d697e936015a252c27a2c25a5711
876285983@qq.com <876285983@qq.com>
2020-02-26 20:15:23 +0800

    test

--- Algorithms--Data_structures/Algorithms--Data_structures/main(主函数)/main.cpp
+++ Algorithms--Data_structures/Algorithms--Data_structures/main(主函数)/main.cpp
@@ -1,14 +1,14 @@
 
 #include <iostream>
-#include "Sorting_algorithm()\Bubble.h"
-#include "Search_algorithm()\Order.h"
+//#include "Sorting_algorithm()\Bubble.h"
+//#include "Search_algorithm()\Order.h"
 
 int main(void)
 {
 
 	std::cout << "Welcome to algorithms and data structures." << std::endl;
-	order();
-	bubble();
+	//order();
+	//bubble();
 
 	std::cin.get();
 }
