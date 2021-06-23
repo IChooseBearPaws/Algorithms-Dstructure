0a85c2bc40e2a2c6d7cf29f2bcf59e79425b9981
I_Choose_Bear_Paws <73148587+IChooseBearPaws@users.noreply.github.com>
2021-06-23 09:38:54 +0800

    repair

--- Algorithms(算法)/Sorting_algorithm(排序算法)/hill_sort.h
+++ Algorithms(算法)/Sorting_algorithm(排序算法)/hill_sort.h
@@ -1,3 +1,21 @@
 #pragma once
+//Ï£¶ûÅÅĞò
 
+template<class T>
 
+void hill_sort(T* arr) {
+	int n = arr.size();
+	int gap = n / 2;
+	while (gap > 0) {
+		for (int i = gap; i < n; ++i) {
+			int tmp = arr[i];
+			int j = i - gap;
+			while (j >= 0 && tmp < arr[j]) {
+				arr[j + gap] = arr[j];
+				j -= gap;
+			}
+			arr[j + gap] = tmp;
+		}
+		gap = gap / 2;
+	}
+}
